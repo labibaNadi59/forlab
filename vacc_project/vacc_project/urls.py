7dd4cf53f41e772c43bb1fe6bd7ce2bddb5e0b8a
mubtahijah02 <mubtahijahanisha02@gmail.com>
2026-04-26 00:46:09 +0600

    Anisha,templates added html file

--- vacc_project/vacc_project/urls.py
+++ vacc_project/vacc_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('vaccination/', include('vaccination.urls')),
+    path('appointments/', include('appointments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
